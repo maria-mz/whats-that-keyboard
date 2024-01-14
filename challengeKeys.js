08cb71f81af4064b5b9455903e9ad540e14907fa
Maria Zaman <mariazaman2@yahoo.com>
2024-01-14 11:24:29 -0800

    add file for retrieving the day's challenge letters
    placeholder until database and lambda func gets implemented. in one file because multiple .js files will access this array

---
+++ challengeKeys.js
@@ -0,0 +1,7 @@
+// TODO: placeholder for now, will get letters from database with lambda func
+const CHALLENGE_KEYS = [
+  'P', 'O', 'I', 'U', 'Y', 'T', 'R', 'E', 'W','Q', 'L', 'K', 'J',
+  'H', 'G', 'F', 'D', 'S', 'A', 'M', 'N', 'B', 'V', 'C', 'X', 'Z'
+];
+
+export default CHALLENGE_KEYS;
--- renderKeyboard.js
+++ renderKeyboard.js
@@ -45,4 +45,4 @@ const createKeyboardRowElmt = (row) => {
 };
 
 
-export { renderKeyboard };
+export default renderKeyboard;
