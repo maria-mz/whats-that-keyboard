39cf8fdd8cba8b361c4ffee95c5eff7ed4135006
Maria Zaman <mariazaman2@yahoo.com>
2024-01-20 10:07:39 -0800

    update naming

--- challengeLetters.js
+++ challengeLetters.js
@@ -1,7 +1,7 @@
 // TODO: placeholder for now, will get letters from database with lambda func
-const CHALLENGE_KEYS = [
+const CHALLENGE_LETTERS = [
   'P', 'O', 'I', 'U', 'Y', 'T', 'R', 'E', 'W','Q', 'L', 'K', 'J',
   'H', 'G', 'F', 'D', 'S', 'A', 'M', 'N', 'B', 'V', 'C', 'X', 'Z'
 ];
 
-export default CHALLENGE_KEYS;
+export default CHALLENGE_LETTERS;
