c49be63cbcb118674c0e4680a1cb39fe22783db8
Maria Zaman <mariazaman2@yahoo.com>
2024-02-20 19:10:51 -0800

    add view for test me button

---
+++ src/views/testMeBtnView.js
@@ -0,0 +1,30 @@
+import { publishEvent } from "../eventBus.js";
+
+/**
+ * @class TestMeBtnView
+ * 
+ * 
+ */
+class TestMeBtnView {
+    constructor() {
+        this.testMeBtn = document.createElement('div');
+
+        this.testMeBtn.textContent = 'Test me!';
+        this.testMeBtn.classList.add('btn');
+
+        this.testMeBtn.addEventListener('click', () => {
+            publishEvent('testMeBtnClicked');
+        });
+    };
+
+    displayTestMeBtn() {
+        const gameInputSection = document.getElementById('gameInput');
+        gameInputSection.appendChild(this.testMeBtn);
+    };
+
+    removeTestMeBtn() {
+        this.testMeBtn.remove()
+    };
+};
+
+export default TestMeBtnView;
