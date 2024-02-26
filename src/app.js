7447f7bb0155bc3f954adbd27fe4b83dac7c264a
Maria Zaman <mariazaman2@yahoo.com>
2024-02-25 17:32:54 -0800

    add results phase controller to App

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import ViewPhaseController from "./controllers/viewPhaseController.js";
 import TestPhaseController from "./controllers/testPhaseController.js";
+import ResultsPhaseController from "./controllers/resultsPhaseController.js";
 
 import GameModel from "./gameModel.js";
 
@@ -15,6 +16,7 @@ class App {
 
         this.viewPhaseController = new ViewPhaseController(this.model);
         this.testPhaseController = new TestPhaseController(this.model);
+        this.resultsPhaseController = new ResultsPhaseController(this.model);
     }
 }
 
